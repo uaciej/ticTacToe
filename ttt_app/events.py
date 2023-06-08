b1367321cad3cd6e5ec36436b40301cfdcc3e9ba
uaciej <amacedmace@gmail.com>
2023-06-08 16:33:31 +0200

    Added credits functionality

--- ttt_app/events.py
+++ ttt_app/events.py
@@ -1,23 +1,58 @@
+from flask import session
 from .extensions import socketio
 from .game_logic import TTTGame
+from .models import Player, db
 from flask_socketio import emit
+
 
 @socketio.on('game_start')
 def handle_game_start():
-    global GAME
-    GAME = TTTGame()
-    emit('move', {'board': GAME.board}, broadcast=True)
+    game = session['game'] = TTTGame() 
+    player = Player.query.filter_by(id=session['player_id']).first()
+    session['player'] = player
+    if player.credits < 3 and player.credits > 0:
+        print('not enough credits')
+        emit('not_enough_credits', {'message': 'Not enough credits'}, broadcast=True)
+        return
+    player.credits -= 3
+    db.session.commit()
+    emit('move', {'board': game.board, 'player':player.id, 'credits': player.credits }, broadcast=True)
 
 @socketio.on('make_move')
 def handle_make_move(data):
     row = data['row']
     col = data['col']
+    game = session['game']
+    player = session['player']
+    game.make_move(row, col)
 
-    GAME.make_move(row, col)
-
-    emit('move', {'board': GAME.board, 'game_over': GAME.game_over, 'winner': GAME.winner}, broadcast=True)
+    emit('move', {'board': game.board, 'game_over': game.game_over, 'winner': game.winner, 'credits': player.credits}, broadcast=True)
 
 @socketio.on('game_over')
 def handle_game_over(data):
-    emit('result', {'winner': data['winner']}, broadcast=True)
+    player = session['player']
+    if data['winner'] == 'X':
+        player = Player.query.filter_by(id=session['player_id']).first()
+        player.credits += 4
+        db.session.commit()
+    if data['winner'] == None:
+        player = Player.query.filter_by(id=session['player_id']).first()
+        player.credits += 3
+        db.session.commit()
+    
+    emit('result', {'winner': data['winner'], 'credits': player.credits}, broadcast=True)
+
+@socketio.on('add_credits')
+def add_credits():
+    player = Player.query.filter_by(id=session['player_id']).first()
+    player.credits = 10
+    db.session.commit()
+    emit('credits_added', {'message': 'Credits added', 'success': True}, broadcast=True)
+
+@socketio.on('start_new_session')
+def start_new_session():
+    player = Player.query.filter_by(id=session['player_id']).first()
+    player.credits = 10
+    db.session.commit()
+    handle_game_start()
 
